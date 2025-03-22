62ac3f4cc86cf7433487f0a33ae08f2f5af0bdc1
FrancisMurayaDev <francismurayadev@gmail.com>
2025-03-22 11:36:22 +0300

    build task app

--- src/Todo/Hero/Hero.jsx
+++ src/Todo/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import TodoInput from "../TodoInput";
 import useTasksStore from "../../store/taskStore";
 import now from "../../utils/now";
+// import Time from "../Time/Time";
 
 const Hero = () => {
   const tasks = useTasksStore((state) => state.tasks);
@@ -10,8 +11,8 @@ const Hero = () => {
   });
   return (
     <section className="hero">
-      <h3>Good {now()}  User</h3>
-      <h2>You have {incomplete} tasks left today</h2>
+      <h3>Good {now()}  User. Welcome to Francis Task App</h3>
+      <h2>You have {incomplete} tasks left today.</h2>
       <TodoInput />
     </section>
   );
--- src/Todo/Todo/Todo.jsx
+++ src/Todo/Todo/Todo.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Hero/Hero";
 import "./Todo.css";
 import TodoItems from "../TodoItems";
+import Time from "../Time/Time";
 
 const Todo = () => {
   return (
     <section className="todo">
       <Hero />
+      <Time />
       <TodoItems />
     </section>
   );
